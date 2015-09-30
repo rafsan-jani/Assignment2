40d898592370cffe2bec05ed71299a5427972407
Md. Rafsan Jani <rafsan.jani@therapservices.net>
2015-09-30 16:00:05 +0600

    Delete CanWalk.java

--- src/CanWalk.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-/**
- * Created by rafsan.jani on 9/22/15.
- */
-@Retention(RetentionPolicy.RUNTIME)
-public @interface CanWalk {
-}
