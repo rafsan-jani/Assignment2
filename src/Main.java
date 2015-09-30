3100f4c562eb1f0ca62c7377a525bf4fad2298a6
rafsan-jani <rafsan.jani@therapservices.net>
2015-09-30 16:14:53 +0600

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args){
-        Testing testing=new Testing();
-        Method[] methods=testing.getClass().getMethods();
+        ValidationFramework validationFramework=new ValidationFramework();
+        Method[] methods=validationFramework.getClass().getMethods();
         for(Method method:methods){
             Test test=method.getAnnotation(Test.class);
             if(test!=null) {
                 System.out.print("Runing " + method.getName() + " :");
                 try {
-                    method.invoke(testing);
+                    method.invoke(validationFramework);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 } catch (InvocationTargetException e) {
